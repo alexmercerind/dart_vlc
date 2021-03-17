03106a8d27d3f7ec7c01a99c092a8e1e00845526
alexmercrind <saini123hitesh@gmail.com>
2021-03-17 15:15:55 +0530

    Added Linux support to example

---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,13 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <dart_vlc/dart_vlc_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) dart_vlc_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "DartVlcPlugin");
+  dart_vlc_plugin_register_with_registrar(dart_vlc_registrar);
+}
