8a318dbe6ae0be8e58b2e3d51c2e21cc57233eed
alexmercerind <saini123hitesh@gmail.com>
2021-03-18 15:34:14 +0530

    Added minimal cli for testing & related files from source control

--- dartvlc/main.cpp
+++ dartvlc/main.cpp
@@ -27,37 +27,91 @@ private:
 	std::map<int, Player*> players;
 };
 
-Players* players = new Players();
 
-int main(int argc, char** argv) {
+Players* players = new Players();
+Devices* devices = new Devices();
+
+
+int main() {
 	Player* player = new Player(0);
-	player->onEvent([player] () -> void {
-		std::cout << std::boolalpha;
-		std::cout << "index       : " << player->state->index << std::endl;
-		std::cout << "position    : " << player->state->position << std::endl;
-		std::cout << "duration    : " << player->state->duration << std::endl;
-		std::cout << "isPlaying   : " << player->state->isPlaying << std::endl;
-		std::cout << "isSeekable  : " << player->state->isSeekable << std::endl;
-		std::cout << "isCompleted : " << player->state->isCompleted << std::endl;
-		std::cout << "isValid     : " << player->state->isValid << std::endl;
-		std::cout << "volume      : " << player->state->volume << std::endl;
-		std::cout << "playlist    : " << "[ " << std::endl;
-		int index = 0;
-		for (Media* media : player->state->medias->medias) {
-			std::cout << "    " << index << ". " << media->mediaType << ", " << media->resource << ", " << std::endl;
-			index++;
+	player->onEvent([]() -> void {});
+	std::string input;
+	while (true) {
+		std::cout << "Listening..." << std::endl;
+		std::cin >> input;
+		if (input == "show") {
+			std::cout << std::boolalpha;
+			std::cout << "index       : " << player->state->index << std::endl;
+			std::cout << "position    : " << player->state->position << std::endl;
+			std::cout << "duration    : " << player->state->duration << std::endl;
+			std::cout << "isPlaylist  : " << player->state->isPlaylist << std::endl;
+			std::cout << "isPlaying   : " << player->state->isPlaying << std::endl;
+			std::cout << "isSeekable  : " << player->state->isSeekable << std::endl;
+			std::cout << "isCompleted : " << player->state->isCompleted << std::endl;
+			std::cout << "isValid     : " << player->state->isValid << std::endl;
+			std::cout << "volume      : " << player->state->volume << std::endl;
+			std::cout << "playlist    : " << "[ " << std::endl;
+			int index = 1;
+			for (Media* media : player->state->medias->medias) {
+				std::cout << "    " << index << ". " << media->mediaType << ", " << media->resource << ", " << std::endl;
+				index++;
+			}
+			std::cout << "]" << std::endl << std::endl;
 		}
-		std::cout << "]" << std::endl << std::endl;
-	});
-	std::vector<Media*> medias;
-	for (int index = 0; index < argc; index++) {
-		medias.emplace_back(
-			Media::file(argv[index])
-		);
+		if (input == "add") {
+			std::string media;
+			std::cout << "Media Location:";
+			std::cin >> media;
+			player->add(
+				Media::file(media)
+			);
+		}
+		if (input == "remove") {
+			int index;
+			std::cout << "Index:";
+			std::cin >> index;
+			player->remove(index);
+		}
+		if (input == "insert") {
+			int index;
+			std::string media;
+			std::cout << "Index:";
+			std::cin >> index;
+			std::cout << "Media Location:";
+			std::cin >> media;
+			player->insert(
+				index,
+				Media::file(media)
+			);
+		}
+		if (input == "play") {
+			player->play();
+		}
+		if (input == "pause") {
+			player->pause();
+		}
+		if (input == "next") {
+			player->next();
+		}
+		if (input == "back") {
+			player->back();
+		}
+		if (input == "jump") {
+			int index;
+			std::cout << "Index:";
+			std::cin >> index;
+			player->jump(index);
+		}
+		if (input == "seek") {
+			int duration;
+			std::cout << "Duration:";
+			std::cin >> duration;
+			player->seek(duration);
+		}
+		if (input == "exit") {
+			break;
+		}
 	}
-	player->open(
-		new Playlist(medias)
-	);
 	std::cin.get();
 	return 0;
 }
