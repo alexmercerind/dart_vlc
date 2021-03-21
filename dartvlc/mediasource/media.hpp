318b7e89801c9d0369bc5f8bc15da494f8652a2d
alexmercerind <saini123hitesh@gmail.com>
2021-03-21 19:08:50 +0530

    Switch to std::promise for making metadata retrieval sync

--- dartvlc/mediasource/media.hpp
+++ dartvlc/mediasource/media.hpp
@@ -11,6 +11,7 @@
 #include <string>
 #include <map>
 #include <filesystem>
+#include <future>
 
 #include <vlcpp/vlc.hpp>
 
@@ -59,44 +60,42 @@ public:
 	}
 
 	void parse(int timeout) {
-		bool isParsed = false;
-		bool* isParsedPointer = &isParsed;
-		VLC::Instance __ = VLC::Instance(0, nullptr);
-		VLC::Media ___ = VLC::Media(__, this->location, VLC::Media::FromLocation);
-		___.eventManager().onParsedChanged(
-			[=](VLC::Media::ParsedStatus status) mutable {
-				*isParsedPointer = true;
+		VLC::Instance _ = VLC::Instance(0, nullptr);
+		VLC::Media __ = VLC::Media(_, this->location, VLC::Media::FromLocation);
+		std::promise<bool>* isParsed = new std::promise<bool>();
+		__.eventManager().onParsedChanged(
+			[isParsed](VLC::Media::ParsedStatus status) -> void {
+				isParsed->set_value(true);
 			}
 		);
-		___.parseWithOptions(
+		__.parseWithOptions(
 			VLC::Media::ParseFlags::Network,
 			timeout
 		);
-		while (!*isParsedPointer)
-			std::this_thread::sleep_for(std::chrono::milliseconds(200));
-		this->metas["title"]       = ___.meta(libvlc_meta_Title);
-		this->metas["artist"]      = ___.meta(libvlc_meta_Artist);
-		this->metas["genre"]       = ___.meta(libvlc_meta_Genre);
-		this->metas["copyright"]   = ___.meta(libvlc_meta_Copyright);
-		this->metas["trackNumber"] = ___.meta(libvlc_meta_TrackNumber);
-		this->metas["description"] = ___.meta(libvlc_meta_Description);
-		this->metas["rating"]      = ___.meta(libvlc_meta_Rating);
-		this->metas["date"]        = ___.meta(libvlc_meta_Date);
-		this->metas["settings"]    = ___.meta(libvlc_meta_Setting);
-		this->metas["url"]         = ___.meta(libvlc_meta_URL);
-		this->metas["language"]    = ___.meta(libvlc_meta_Language);
-		this->metas["nowPlaying"]  = ___.meta(libvlc_meta_NowPlaying);
-		this->metas["encodedBy"]   = ___.meta(libvlc_meta_EncodedBy);
-		this->metas["artworkUrl"]  = ___.meta(libvlc_meta_ArtworkURL);
-		this->metas["trackTotal"]  = ___.meta(libvlc_meta_TrackTotal);
-		this->metas["director"]    = ___.meta(libvlc_meta_Director);
-		this->metas["season"]      = ___.meta(libvlc_meta_Season);
-		this->metas["episode"]     = ___.meta(libvlc_meta_Episode);
-		this->metas["actors"]      = ___.meta(libvlc_meta_Actors);
-		this->metas["albumArtist"] = ___.meta(libvlc_meta_AlbumArtist);
-		this->metas["discNumber"]  = ___.meta(libvlc_meta_DiscNumber);
-		this->metas["discTotal"]   = ___.meta(libvlc_meta_DiscTotal);
-		this->metas["duration"]    = std::to_string(___.duration());
+		bool ___ = isParsed->get_future().get();
+		this->metas["title"]       = __.meta(libvlc_meta_Title);
+		this->metas["artist"]      = __.meta(libvlc_meta_Artist);
+		this->metas["genre"]       = __.meta(libvlc_meta_Genre);
+		this->metas["copyright"]   = __.meta(libvlc_meta_Copyright);
+		this->metas["trackNumber"] = __.meta(libvlc_meta_TrackNumber);
+		this->metas["description"] = __.meta(libvlc_meta_Description);
+		this->metas["rating"]      = __.meta(libvlc_meta_Rating);
+		this->metas["date"]        = __.meta(libvlc_meta_Date);
+		this->metas["settings"]    = __.meta(libvlc_meta_Setting);
+		this->metas["url"]         = __.meta(libvlc_meta_URL);
+		this->metas["language"]    = __.meta(libvlc_meta_Language);
+		this->metas["nowPlaying"]  = __.meta(libvlc_meta_NowPlaying);
+		this->metas["encodedBy"]   = __.meta(libvlc_meta_EncodedBy);
+		this->metas["artworkUrl"]  = __.meta(libvlc_meta_ArtworkURL);
+		this->metas["trackTotal"]  = __.meta(libvlc_meta_TrackTotal);
+		this->metas["director"]    = __.meta(libvlc_meta_Director);
+		this->metas["season"]      = __.meta(libvlc_meta_Season);
+		this->metas["episode"]     = __.meta(libvlc_meta_Episode);
+		this->metas["actors"]      = __.meta(libvlc_meta_Actors);
+		this->metas["albumArtist"] = __.meta(libvlc_meta_AlbumArtist);
+		this->metas["discNumber"]  = __.meta(libvlc_meta_DiscNumber);
+		this->metas["discTotal"]   = __.meta(libvlc_meta_DiscTotal);
+		this->metas["duration"]    = std::to_string(__.duration());
 	}
 
 	std::string mediaSourceType() {
