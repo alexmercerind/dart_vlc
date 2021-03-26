557486198a41b6e3f1b5f23ea52a37b7abfb4357
alexmercrind <saini123hitesh@gmail.com>
2021-03-27 01:15:36 +0530

    Final changes for video frame callbacks

--- dartvlc/main.cpp
+++ dartvlc/main.cpp
@@ -8,32 +8,8 @@
  * GNU Lesser General Public License v2.1
  */
 
-#include <iostream>
-#include <fstream>
-#include <ios>
-
 #include "player.hpp"
 #include "device.hpp"
 
 
-int main() {
-    int index = 0;
-    int* indexPointer = &index;
-    Player* player = new Player(0);
-    player->onEvent([]() -> void {});
-    player->onVideo(
-        480,
-        320,
-        [=](uint8_t* frame) -> void {
-            (*indexPointer)++;
-            std::fstream file("/home/alexmercerind/frames/frame" + std::to_string((*indexPointer)) + ".RAW", std::ios::out | std::ios::binary);
-            file.write((char*)frame, 480 * 320 * 4 * 8);
-            file.close();
-        }
-    );
-    player->open(
-        Media::file(0, "/home/alexmercerind/movie.mkv")
-    );
-    std::cin.get();
-    return 0;
-}
+/* libVLC++ wrapper for dart_vlc */
