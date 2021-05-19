60b72cb9a1ed93110e9ec50441ba35399074fb6b
alexmercerind <saini123hitesh@gmail.com>
2021-05-19 17:23:53 +0530

    Fixed not accessible exception

--- dartvlc/equalizer.hpp
+++ dartvlc/equalizer.hpp
@@ -91,13 +91,15 @@ public:
 	}
 
 	int createEmpty() {
-		this->equalizers[static_cast<int>(this->equalizers.size())] = new Equalizer();
-		return static_cast<int>(this->equalizers.size());
+		int id = static_cast<int>(this->equalizers.size());
+		this->equalizers[id] = new Equalizer();
+		return id;
 	}
 
 	int createMode(EqualizerMode mode) {
-		this->equalizers[static_cast<int>(this->equalizers.size())] = new Equalizer(mode);
-		return static_cast<int>(this->equalizers.size());
+		int id = static_cast<int>(this->equalizers.size());
+		this->equalizers[id] = new Equalizer(mode);
+		return id;
 	}
 
 private:
