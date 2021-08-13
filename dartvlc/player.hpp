2588b2a3c2325f68eef51543372217450a125f74
alexmercerind <saini123hitesh@gmail.com>
2021-08-14 02:15:26 +0530

    [wrapper] Add Player::OnVideoDimension listener

--- dartvlc/player.hpp
+++ dartvlc/player.hpp
@@ -14,20 +14,21 @@
 #ifndef Player_HEADER
 #define Player_HEADER
 
-auto TO_CHARARRAY = [](std::vector<std::string>* vector) -> char** {
-  size_t size = vector->size();
+auto TO_CHARARRAY = [](std::vector<std::string>& vector) -> char** {
+  size_t size = vector.size();
   char** array = new char*[size];
-  for (int32_t i = 0; i < size; i++) array[i] = (*vector)[i].data();
+  for (int32_t i = 0; i < size; i++) array[i] = vector[i].data();
   return array;
 };
 
 class Player : public PlayerSetters {
  public:
   Player(std::vector<std::string> cmd_arguments = {}) {
+    cmd_arguments.emplace_back("--no-video");
     if (cmd_arguments.empty()) {
       vlc_instance_ = VLC::Instance(0, nullptr);
     } else {
-      char** args = TO_CHARARRAY(&cmd_arguments);
+      char** args = TO_CHARARRAY(cmd_arguments);
       vlc_instance_ =
           VLC::Instance(static_cast<int32_t>(cmd_arguments.size()), args);
       delete[] args;
