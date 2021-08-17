d7b554d44c0eecf439f01d92d0c161f4e9f9ca56
alexmercerind <saini123hitesh@gmail.com>
2021-08-17 20:36:37 +0530

    [wrapper] Add external Dart API headers & update CMake

--- dartvlc/api/callbackmanager.h
+++ dartvlc/api/callbackmanager.h
@@ -7,89 +7,19 @@
 #include <vector>
 
 #include "base.h"
-
-typedef int64_t Dart_Port;
-
-typedef enum {
-  Dart_TypedData_kByteData = 0,
-  Dart_TypedData_kInt8,
-  Dart_TypedData_kUint8,
-  Dart_TypedData_kUint8Clamped,
-  Dart_TypedData_kInt16,
-  Dart_TypedData_kUint16,
-  Dart_TypedData_kInt32,
-  Dart_TypedData_kUint32,
-  Dart_TypedData_kInt64,
-  Dart_TypedData_kUint64,
-  Dart_TypedData_kFloat32,
-  Dart_TypedData_kFloat64,
-  Dart_TypedData_kFloat32x4,
-  Dart_TypedData_kInvalid
-} Dart_TypedData_Type;
-
-typedef struct _Dart_WeakPersistentHandle* Dart_WeakPersistentHandle;
-
-typedef void (*Dart_WeakPersistentHandleFinalizer)(
-    void* isolate_callback_data, Dart_WeakPersistentHandle handle, void* peer);
-
-typedef enum {
-  Dart_CObject_kNull = 0,
-  Dart_CObject_kBool,
-  Dart_CObject_kInt32,
-  Dart_CObject_kInt64,
-  Dart_CObject_kDouble,
-  Dart_CObject_kString,
-  Dart_CObject_kArray,
-  Dart_CObject_kTypedData,
-  Dart_CObject_kExternalTypedData,
-  Dart_CObject_kSendPort,
-  Dart_CObject_kCapability,
-  Dart_CObject_kUnsupported,
-  Dart_CObject_kNumberOfTypes
-} Dart_CObject_Type;
-
-typedef struct _Dart_CObject {
-  Dart_CObject_Type type;
-  union {
-    bool as_bool;
-    int32_t as_int32;
-    int64_t as_int64;
-    double as_double;
-    char* as_string;
-    struct {
-      Dart_Port id;
-      Dart_Port origin_id;
-    } as_send_port;
-    struct {
-      int64_t id;
-    } as_capability;
-    struct {
-      intptr_t length;
-      struct _Dart_CObject** values;
-    } as_array;
-    struct {
-      Dart_TypedData_Type type;
-      intptr_t length;
-      uint8_t* values;
-    } as_typed_data;
-    struct {
-      Dart_TypedData_Type type;
-      intptr_t length;
-      uint8_t* data;
-      void* peer;
-      Dart_WeakPersistentHandleFinalizer callback;
-    } as_external_typed_data;
-  } value;
-} Dart_CObject;
-
-typedef bool (*Dart_PostCObjectType)(Dart_Port port_id, Dart_CObject* message);
+#include "api/dartmanager.h"
+#include "dart_api_dl.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-Dart_PostCObjectType g_dart_post_C_object = NULL;
+typedef bool (*Dart_PostCObjectType)(Dart_Port port_id, Dart_CObject* message);
+
+Dart_PostCObjectType g_dart_post_C_object;
 Dart_Port g_callback_port;
+
+DLLEXPORT void DartInitializeApiDL(void* data) { Dart_InitializeApiDL(data); }
 
 DLLEXPORT void RegisterDartPostCObject(
     Dart_PostCObjectType dart_post_C_object) {
@@ -115,7 +45,7 @@ void CallbackStringArray(int32_t length, char** values) {
   auto value_objects =
       std::unique_ptr<Dart_CObject[]>(new Dart_CObject[length]);
   auto value_object_refs =
-      std::unique_ptr<Dart_CObject*[]>(new Dart_CObject*[length]);
+      std::unique_ptr<Dart_CObject* []>(new Dart_CObject*[length]);
 
   for (int32_t i = 0; i < length; i++) {
     Dart_CObject* value_object = &value_objects[i];
@@ -135,7 +65,7 @@ void CallbackStringArray(const std::vector<std::string>& values) {
   auto value_objects =
       std::unique_ptr<Dart_CObject[]>(new Dart_CObject[length]);
   auto value_object_refs =
-      std::unique_ptr<Dart_CObject*[]>(new Dart_CObject*[length]);
+      std::unique_ptr<Dart_CObject* []>(new Dart_CObject*[length]);
 
   for (int32_t i = 0; i < length; i++) {
     Dart_CObject* value_object = &value_objects[i];
