29a895cd7a24cb4955c62242b3c730fee60696b9
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-04-03 19:16:08 +0000

    fix: missing export

--- ffi/lib/dart_vlc_ffi.dart
+++ ffi/lib/dart_vlc_ffi.dart
@@ -1,17 +1,26 @@
-/*
- * dart_vlc: A media playback library for Dart & Flutter. Based on libVLC & libVLC++.
- * 
- * Hitesh Kumar Saini
- * https://github.com/alexmercerind
- * alexmercerind@gmail.com
- * 
- * GNU Lesser General Public License v2.1
- */
+// This file is a part of dart_vlc (https://github.com/alexmercerind/dart_vlc)
+//
+// Copyright (C) 2021-2022 Hitesh Kumar Saini <saini123hitesh@gmail.com>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 export 'package:dart_vlc_ffi/src/player.dart';
 export 'package:dart_vlc_ffi/src/equalizer.dart';
 export 'package:dart_vlc_ffi/src/broadcast.dart';
 export 'package:dart_vlc_ffi/src/chromecast.dart';
+export 'package:dart_vlc_ffi/src/record.dart';
 export 'package:dart_vlc_ffi/src/device.dart';
 export 'package:dart_vlc_ffi/src/player_state/player_state.dart';
 export 'package:dart_vlc_ffi/src/media_source/media_source.dart';
