c926954ee3c55190709d48de2312f7cd17d074c1
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-05-05 18:34:49 +0530

    example app

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <dart_vlc/dart_vlc_plugin.h>
+#include <flutter_native_view/flutter_native_view_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   DartVlcPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("DartVlcPlugin"));
+  FlutterNativeViewPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterNativeViewPlugin"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   dart_vlc
+  flutter_native_view
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
