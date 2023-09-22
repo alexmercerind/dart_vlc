a2a43a79d892f9b02b9953b1da6b4bf09ef443c3
luisdm921 <86019062+luisdm921@users.noreply.github.com>
2023-09-22 14:36:46 -0600

    Update broadcast.cc

--- core/broadcast/broadcast.cc
+++ core/broadcast/broadcast.cc
@@ -33,7 +33,7 @@ void Broadcast::Start() {
        << ", vb=" << configuration_->vb()
        << ", acodec=" << configuration_->acodec()
        << ", ab=" << configuration_->ab()
-       << ", vfilter=transform" // Agregamos vfilter=mirror aquí
+       << ", vfilter=transform:hflip" // Agregamos vfilter=mirror aquí
        << "}:std{access=" << configuration_->access()
        << ", mux=" << configuration_->mux()
        << ", dst=" << configuration_->dst();
