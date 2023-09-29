36fe7bb90f71bf5e4d999ed0010f28f29fbdaf50
luisdm921 <86019062+luisdm921@users.noreply.github.com>
2023-09-28 18:32:01 -0600

    Update broadcast.cc

--- core/broadcast/broadcast.cc
+++ core/broadcast/broadcast.cc
@@ -29,15 +29,14 @@ Broadcast::Broadcast(std::shared_ptr<Media> media,
 
 void Broadcast::Start() {
   std::stringstream sout;
-sout << "#duplicate{dst='transcode{vcodec=" << configuration_->vcodec()
-     << ", vb=" << configuration_->vb()
-     << ", acodec=" << configuration_->acodec()
-     << ", ab=" << configuration_->ab()
-     << "}:std{access=" << configuration_->access()
-     << ", mux=" << configuration_->mux()
-     << ", dst=" << configuration_->dst()
-     << "}',dst=display{vout=opengl, delay=0}}";
-;
+ sout << "#transcode{vcodec=" << configuration_->vcodec()
+       << ", vb=" << configuration_->vb()
+       << ", acodec=" << configuration_->acodec()
+       << ", ab=" << configuration_->ab()
+       << "}:std{access=" << configuration_->access()
+       << ", mux=" << configuration_->mux()
+       << ", dst=" << configuration_->dst();
+
 
 
 
