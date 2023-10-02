ffc650acd3ec0868e497823556d2fc2f97d71179
luisdm921 <86019062+luisdm921@users.noreply.github.com>
2023-10-02 13:29:29 -0600

    Update broadcast.cc

--- core/broadcast/broadcast.cc
+++ core/broadcast/broadcast.cc
@@ -49,7 +49,7 @@ sout << "#duplicate{dst='transcode{vcodec=" << configuration_->vcodec()
 
   // Modifica la línea siguiente para usar dshow:// en lugar de media_->location().c_str()
   libvlc_vlm_add_broadcast(vlc_instance_.get(), "dshow:// ", "dshow://", sout.str().c_str(), 0, nullptr, true, false);
- // libvlc_vlm_play_media(vlc_instance_.get(), "dshow:// ");
+  libvlc_vlm_play_media(vlc_instance_.get(), "dshow:// ");
 }
 
 
