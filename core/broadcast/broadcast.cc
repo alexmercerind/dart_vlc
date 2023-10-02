bccca91327462f67c581daf398ae8931cf62ea26
luisdm921 <86019062+luisdm921@users.noreply.github.com>
2023-10-02 14:39:45 -0600

    Update broadcast.cc

--- core/broadcast/broadcast.cc
+++ core/broadcast/broadcast.cc
@@ -36,7 +36,7 @@ sout << "#duplicate{dst='transcode{vcodec=" << configuration_->vcodec()
      << "}:std{access=" << configuration_->access()
      << ", mux=" << configuration_->mux()
      << ", dst=" << configuration_->dst()
-     << "}',dst=display{title="Mi Reproductor Personalizado"}}";
+     << "}',dst=display{fullscreen=no}}";
 
 
 
