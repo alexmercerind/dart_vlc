04c80a087f6232f7024d88beb4f4e1227745fe31
luisdm921 <86019062+luisdm921@users.noreply.github.com>
2023-10-02 14:56:03 -0600

    Update broadcast.cc

--- core/broadcast/broadcast.cc
+++ core/broadcast/broadcast.cc
@@ -36,7 +36,7 @@ sout << "#duplicate{dst='transcode{vcodec=" << configuration_->vcodec()
      << "}:std{access=" << configuration_->access()
      << ", mux=" << configuration_->mux()
      << ", dst=" << configuration_->dst()
-     << "}',dst=display{fullscreen=no}}";
+     << "}',dst=display}";
 
 
 
