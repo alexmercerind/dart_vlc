161f226d1cf1fc945b74fd82f061620d84843a52
luisdm921 <86019062+luisdm921@users.noreply.github.com>
2023-10-17 14:30:47 -0600

    Update win32_window.h

--- windows/include/dart_vlc/win32_window.h
+++ windows/include/dart_vlc/win32_window.h
@@ -26,7 +26,7 @@
 #include <Windows.h>
 #include <stdint.h>
 
-HWND CreateWin32Window(bool fullscreen = true);
+HWND CreateWin32Window();
 
 LRESULT CALLBACK Win32WindowProc(HWND window, UINT message, WPARAM wparam,
                                  LPARAM lparam);
