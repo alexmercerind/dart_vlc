de28d5776b23af8dbe42dcc23a689c6146cf5f95
luisdm921 <86019062+luisdm921@users.noreply.github.com>
2023-10-17 12:51:00 -0600

    Update win32_window.cc

--- windows/win32_window.cc
+++ windows/win32_window.cc
@@ -1,47 +1,16 @@
-HWND CreateWin32Window(const wchar_t* title, int width, int height, bool fullscreen = false) {
-    // Valores predeterminados
-    const wchar_t* defaultTitle = L"Mi Ventana Win32";
-    HFONT defaultFont = NULL; // Puedes establecer una fuente personalizada aquí
-    HICON defaultIcon = LoadIcon(NULL, IDI_APPLICATION); // Ícono predeterminado
-    HMENU defaultMenu = NULL; // Puedes configurar una barra de menú personalizada aquí
+#ifndef WIN32_WINDOW_H_
+#define WIN32_WINDOW_H_
 
-    // Utiliza los valores predeterminados si no se proporcionan valores personalizados
-    if (title == nullptr) {
-        title = defaultTitle;
-    }
-    if (width == 0 || height == 0) {
-        width = GetSystemMetrics(SM_CXSCREEN); // Ancho de la pantalla
-        height = GetSystemMetrics(SM_CYSCREEN); // Alto de la pantalla
-    }
+#ifndef UNICODE
+#define UNICODE
+#endif
 
-    // Resto de tu código para crear la ventana...
+#include <Windows.h>
+#include <stdint.h>
 
-    // Si se selecciona el modo de pantalla completa
-    if (fullscreen) {
-        // Cambiar la resolución de la pantalla
-        DEVMODE dmScreenSettings;
-        memset(&dmScreenSettings, 0, sizeof(dmScreenSettings));
-        dmScreenSettings.dmSize = sizeof(dmScreenSettings);
-        dmScreenSettings.dmPelsWidth = width;
-        dmScreenSettings.dmPelsHeight = height;
-        dmScreenSettings.dmBitsPerPel = 32; // Puedes ajustar esto según tu preferencia
-        dmScreenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+HWND CreateWin32Window();
 
-        if (ChangeDisplaySettings(&dmScreenSettings, CDS_FULLSCREEN) != DISP_CHANGE_SUCCESSFUL) {
-            // No se pudo cambiar a pantalla completa, manejar el error
-        }
-    }
+LRESULT CALLBACK Win32WindowProc(HWND window, UINT message, WPARAM wparam,
+                                 LPARAM lparam);
 
-    // Crea una estructura WindowData con los valores predeterminados
-    WindowData* windowData = new WindowData;
-    windowData->hFont = defaultFont;
-    windowData->hIcon = defaultIcon;
-    windowData->hMenu = defaultMenu;
-
-    // Asigna la estructura WindowData a la propiedad GWLP_USERDATA
-    SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)windowData);
-
-    // Resto de la creación de la ventana...
-
-    return hwnd;
-}
+#endif
