9773626c72781e78837aa4f0f724a444f9d2564d
luisdm921 <86019062+luisdm921@users.noreply.github.com>
2023-10-17 13:32:53 -0600

    Update win32_window.h

--- windows/include/dart_vlc/win32_window.h
+++ windows/include/dart_vlc/win32_window.h
@@ -26,7 +26,7 @@
 #include <Windows.h>
 #include <stdint.h>
 
-HWND CreateWin32Window();
+HWND CreateWin32Window(true);
 
 LRESULT CALLBACK Win32WindowProc(HWND window, UINT message, WPARAM wparam,
                                  LPARAM lparam);
