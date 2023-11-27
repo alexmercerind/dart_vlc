304c890bcee45dcb962c0ba03626be9127ed1522
luisdm921 <86019062+luisdm921@users.noreply.github.com>
2023-11-27 12:37:55 -0600

    Update broadcast.cc

--- core/broadcast/broadcast.cc
+++ core/broadcast/broadcast.cc
@@ -36,7 +36,7 @@ void Broadcast::Start() {
        << "}:std{access=" << configuration_->access()
        << ", mux=" << configuration_->mux()
        << ", dst=" << configuration_->dst()
-       << "}',dst=display}";
+       << "}',dst=vlc}";
 
 
 
